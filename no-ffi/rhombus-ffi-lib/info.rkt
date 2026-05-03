6884bd70f6dcef7b80dcca9bf6813ff4b511c5ec
Matthew Flatt <mflatt@racket-lang.org>
2026-05-03 16:46:15 -0600

    add `catalog-maker`
    The `catalog-maker/main` program relies on `raco-cross`. It creates a
    catalog of built Rhombus packages compatible with a particular Racket
    version (i.e., can be installed with only a "recompile" step, which
    converts a machine-independent fully-expanded form into machine code).
    The output "/tmp/catalog-maker/site" is meant to be uploaded somewhere
    so that "<version>/catalog/" can be used as a package catalog for a
    Racket <version> installation.

---
+++ no-ffi/rhombus-ffi-lib/info.rkt
@@ -0,0 +1,12 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps
+  '("base"))
+
+(define pkg-desc "implementation (no documentation) part of \"rhombus-ffi\"")
+
+(define license '(Apache-2.0 OR MIT))
+
+(define version "0.1")
---
+++ no-ffi/rhombus-ffi/rhombus/ffi/scribblings/rhombus-ffi.scrbl
@@ -0,0 +1,6 @@
+#lang rhombus/scribble/manual
+
+@title(~category: #'#{core}){Rhombus FFI: Foreign-Function Interface}
+
+The Rhombus foreign-function interface is available only with Racket
+version 9.2 and later.
